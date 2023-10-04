4cd760cbc821351916e5953da681fc2bcf12b2a7
Dambldory <144476729+Dambldory@users.noreply.github.com>
2023-10-04 19:33:35 +0300

    Add files via upload

--- Source.c
+++ Source.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <locale.h>
 #define d 2.54
+#define d1 2.7076
+#define g 6.67e-11
 void main()
 {
 	setlocale(LC_CTYPE, "RUS");
@@ -14,21 +16,33 @@ void main()
 	printf("Сумма этих чисел: %d \nРазность этих чисел: %d\nПроизведение чисел: %d\n", num1 + num, num1 - num, num1 * num);
 	printf("Частное чисел: %.3f\nОстаток деления: %d\n", (float)num1 / num,num1%num);
 	*/
-	/*int dym;
-	float result;
+	int dym;
+	float result, result1;
 	puts("Введите значение:\n");
 	scanf("%d", &dym);
 	result = d * dym;
-	printf("%d дюймов - это %.1f сантиметров", dym, result);
-	*/
-	int a, b;
+	result1 = d1 * dym;
+	printf("%d дюймов - это %.1f сантиметров\n", dym, result);
+	printf("%d старолитовских дюймов - это %.1f сантиметров", dym, result1);
+	
+	/*int a, b;
 	puts("Введите число");
 	scanf("%d\n %d", &a, &b);
-	//printf("%d, %d", a, b);
-	printf(" __________________\n");
+	printf("___________________\n");
 	printf("| a*b | a+b | a-b |\n");
-	printf(" __________________\n");
-	printf("|% d * %d | % d + % d | % d - % d | \n", a, b, a, b, a, b);
-	printf(" __________________\n");
+	printf("-------------------\n");
+	printf("|%2d*%-2d|%2d+%-2d|%2d-%-2d|\n", a, b, a, b, a, b);
+	printf("-------------------\n");
+	printf("|%3d  |%3d  |%3d  |\n", a * b, a + b, a - b);
+	printf("-------------------\n");*/
+	/*int a, b, c;
+	puts("Введите массу первого тела (в кг):");
+	scanf("%d", &a);
+	puts(" \nВведите массу второго тела (в кг):");
+	scanf("%d", &b);
+	puts("\nВведите расстояние между ними (в метрах):");
+	scanf("%d", &c);
+	puts("\nСила притяжения между телами равна:");
+	printf("%e Ньютонов",g * ((a * b) / (c * c)));*/
 
 }
