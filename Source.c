e655860a202f6a2aa1bf59ec6becd01573990957
Dambldory <144476729+Dambldory@users.noreply.github.com>
2023-10-05 12:36:22 +0300

    Add files via upload

--- Source.c
+++ Source.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <locale.h>
-#include<stdlib.h>
+#include <stdlib.h>
 #define d 2.54
 #define d1 2.7076
-#define g 6.67e-11
+#define g 0.0000000000667
+double dz(float x, float y, float z)
+{
+	double result;
+	result = g * x * y / (z * z);
+	return result;
+}
 void main()
 {
 	setlocale(LC_CTYPE, "RUS");
@@ -25,26 +31,27 @@ void main()
 	result1 = d1 * dym;
 	printf("%d дюймов - это %.1f сантиметров\n", dym, result);
 	printf("%d старолитовских дюймов - это %.1f сантиметров", dym, result1);*/
-	
-	int a, b;
+
+	/*int a, b;
 	puts("Введите число");
 	scanf("%d\n %d", &a, &b);
 	printf("___________________\n");
 	printf("| a*b | a+b | a-b |\n");
 	printf("-------------------\n");
-	printf("|%2d*%-2d|%2d+%-2d|%2d-%-2d|\n", a, b, a, b, a, b);
+	printf("|%2f*%-2f|%2f+%-2f|%2f-%-2f|\n", a, b, a, b, a, b);
 	printf("-------------------\n");
-	printf("|%3d  |%3d  |%3d  |\n", a * b, a + b, a - b);
+	printf("|%3f  |%3f  |%3f  |\n", a * b, a + b, a - b);
 	printf("-------------------\n");
-	system("pause");
-	/*int a, b, c;
+	system("pause");*/
+	float a, b, c;
+	double f;
 	puts("Введите массу первого тела (в кг):");
 	scanf("%d", &a);
 	puts(" \nВведите массу второго тела (в кг):");
 	scanf("%d", &b);
 	puts("\nВведите расстояние между ними (в метрах):");
 	scanf("%d", &c);
-	puts("\nСила притяжения между телами равна:");
-	printf("%e Ньютонов",g * ((a * b) / (c * c)));*/
-
+	f=dz(a,b,c);
+	printf("\nСила притяжения между телами равна: %e\n", dz(a,b,c));
+	system("pause");
 }
